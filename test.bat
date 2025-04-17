cf09651c160383d8ef81f324db4da18935fcd99c
unknown <vihashinidurga2gmail.com>
2025-04-17 10:36:23 +0530

    Edit changes in html and bat file

--- test.bat
+++ test.bat
@@ -1,3 +1,8 @@
-#!/bin/bash
-# test.sh
-grep -q "Hello this is Durgavihashini!!" index.html
+@echo off
+findstr /C:"Hello this is Durgavihashini!!" index.html >nul
+if errorlevel 1 (
+    echo Test failed: Text not found.
+    exit /b 1
+) else (
+    echo Test passed: Text found.
+)
